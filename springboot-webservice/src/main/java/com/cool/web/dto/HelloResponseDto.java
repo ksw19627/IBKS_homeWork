35bad479bc2022cf4f780cf3408b6b7b7e535c79
cool <cool@ibk.com>
2024-05-21 14:01:05 +0900

    Chapter#2 (진행중 - 미완 코드)

--- springboot-webservice/src/main/java/com/cool/web/dto/HelloResponseDto.java
+++ springboot-webservice/src/main/java/com/cool/web/dto/HelloResponseDto.java
@@ -1,16 +1,18 @@
 package com.cool.web.dto;
 
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 
 @Getter
 @RequiredArgsConstructor
+@NoArgsConstructor
 @GetMapping("hello/dto")
-public class HelloResponseDto {
+public  HelloResponseDto {
     private final String name;
     private final int amount;
-    }
 }
--- springboot-webservice/src/test/java/com/cool/web/HelloControllerTest.java
+++ springboot-webservice/src/test/java/com/cool/web/HelloControllerTest.java
@@ -1,11 +1,14 @@
 package com.cool.web;
 
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 
 @RunWith(SpringRunner.class) // ①
@@ -20,7 +23,7 @@ public class HelloControllerTest {
     @Test
     public void hello_return() throws Exception {
         String hello = "hello";
-        mvc.perform(get("/hello")) // ➄
+        mvc.perform(MockMvcRequestBuilders.get("/hello")) // ➄
                 .andExpect(status().isOk()) // ➅
                 .andExpect(content().string(hello)); // ➆
     }
