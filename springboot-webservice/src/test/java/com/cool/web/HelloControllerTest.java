76acf4f87c1893af70d299bd1de70abc1d74d7cc
cool <cool@ibk.com>
2024-05-22 13:03:28 +0900

    Chapter#2 진행 중 - DTOTest 구현 중

--- springboot-webservice/src/test/java/com/cool/web/HelloControllerTest.java
+++ springboot-webservice/src/test/java/com/cool/web/HelloControllerTest.java
@@ -4,21 +4,18 @@ import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.test.context.junit4.SpringRunner;
+
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 
-@RunWith(SpringRunner.class) // ①
+@RunWith(org.springframework.test.context.junit4.SpringRunner.class) // ①
 @WebMvcTest // ②
 public class HelloControllerTest {
     @Autowired //  ➂
     private MockMvc mvc; //  ➃
 
-    public HelloControllerTest(MockMvc mvc){
-
-    }
     @Test
     public void hello_return() throws Exception {
         String name = "hello";
